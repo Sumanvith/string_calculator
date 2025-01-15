ded26906c9f65080904313c9c696d44bc50ed1ca
Sumanvith <dellbilla@gmail.com>
2025-01-15 18:26:08 +0530

    Support multiple custom delimiters with length > 1

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -2,10 +2,16 @@ def add(numbers):
     if not numbers:
         return 0
     if numbers.startswith("//"):
-        delimiter, numbers = numbers[2:].split("\n", 1)
-        numbers = numbers.replace(delimiter, ",")
+        delimiters_section, numbers = numbers[2:].split("\n", 1)
+        delimiters = []
+        if delimiters_section.startswith("[") and delimiters_section.endswith("]"):
+            delimiters = delimiters_section[1:-1].split("][")
+        else:
+            delimiters = [delimiters_section]
+        for delimiter in delimiters:
+            numbers = numbers.replace(delimiter, ",")
     numbers = numbers.replace("\n", ",")
-    number_list = list(map(int, numbers.split(",")))
+    number_list = list(map(int, numbers.split(","))) if numbers else []
     negatives = [n for n in number_list if n < 0]
     if negatives:
         raise ValueError(f"Negatives not allowed: {','.join(map(str, negatives))}")
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,6 +1,11 @@
 import pytest
 from src.string_calculator import add
 
-def test_ignore_large_numbers():
-    assert add("2,1001") == 2
+def test_multiple_custom_delimiters():
+    assert add("//[***][%%]\n1***2%%3") == 6
 
+def test_multiple_custom_delimiters_with_length():
+    assert add("//[***][%%%]\n1***2%%%3") == 6
+
+def test_multiple_custom_delimiters_and_large_numbers():
+    assert add("//[;;][##]\n2;;1001##3") == 5
